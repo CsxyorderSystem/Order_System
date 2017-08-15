4d2df1471db49c4ff3bf9822fb1a34cf0a493b20
206170 <704688407@qq.com>
2017-08-15 16:49:50 +0800

    V0.251 完成User的service设置

--- src/edu/csxy/order/Dao_impl/user_Dao_impl.java
+++ src/edu/csxy/order/Dao_impl/user_Dao_impl.java
@@ -9,15 +9,18 @@ public class user_Dao_impl extends BaseDao<User_Bean> implements user_Dao {
 	@Override
 	public boolean changePassword(String U_phone, String U_password, String U_id) {
 		// TODO Auto-generated method stub
-		return false;
+		//改密码
+		String sql="update user set U_password=? Where U_phone=? and U_id=? ";
+		update(sql, U_password,U_phone,U_id);
+		return true;
 	}
 
 	@Override
 	public String checkUser(String U_phone, String U_password) {
 		//登录
-		String sql= "select * from user where U_phone=? and U_password=?";
-		query(sql,U_phone,U_password);
 		User_Bean user=new User_Bean();
+		String sql= "select * from user where U_phone=? and U_password=?";//获取账号密码
+		query(sql,U_phone,U_password);
 		String U_id=user.getU_id();
 		// TODO Auto-generated method stub
 		return U_id;
@@ -26,7 +29,7 @@ public class user_Dao_impl extends BaseDao<User_Bean> implements user_Dao {
 	@Override
 	public boolean sign(String U_phone, String U_password, String U_id) {
 		//注册用户
-		String sql="insert into user values "+"(?,?,?)";
+		String sql="insert into user values "+"(?,?,?)"; //插入注册操作，但需要先进行检查是否被注册
 		insert(sql,U_phone,U_password,U_id);
 		// TODO Auto-generated method stub
 		return true;
@@ -35,7 +38,11 @@ public class user_Dao_impl extends BaseDao<User_Bean> implements user_Dao {
 	@Override
 	public String alreadysign(String U_phone) {
 		// TODO Auto-generated method stub
-		return null;
+		//检查是否注册
+		User_Bean user=new User_Bean();//获取bean的内容
+		String sql="select * from user where U_phone=?";//进行查询注册操作
+		query(sql, U_phone);
+		return user.getU_id();//若被注册，返回U_id
 	}
 
 
